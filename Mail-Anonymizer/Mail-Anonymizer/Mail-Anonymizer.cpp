c46ec1c89474c9e3eff0da05e40ede465b71beb9
Raphael@Linux <akekahn@gmail.com>
2013-07-03 09:06:24 -0400

    Simple Sha256 with openssl

--- Mail-Anonymizer/Mail-Anonymizer/Mail-Anonymizer.cpp
+++ Mail-Anonymizer/Mail-Anonymizer/Mail-Anonymizer.cpp
@@ -6,8 +6,21 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello World";
-	return 2;
+int main() {
+    unsigned char digest[SHA256_DIGEST_LENGTH];
+    const char* string = "hello world"; 
+
+    SHA256_CTX ctx;
+    SHA256_Init(&ctx);
+    SHA256_Update(&ctx, string, strlen(string));
+    SHA256_Final(digest, &ctx);
+
+    char mdString[SHA256_DIGEST_LENGTH*2+1];
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+
+    printf("SHA256 digest: %s\n", mdString);
+
+
+    return 0;
 }
--- Mail-Anonymizer/Mail-Anonymizer/stdafx.h
+++ Mail-Anonymizer/Mail-Anonymizer/stdafx.h
@@ -14,4 +14,6 @@
 
 
 // TODO: reference additional headers your program requires here
-#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <openssl/sha.h>
