22d852470a2d40abe6cabd4acfb5d90da6ce06d4
Raphael@Windows <r.ochsenbein@akehir.com>
2013-07-08 14:06:26 -0400

    Release 1.0

--- Mail-Anonymizer/Mail-Anonymizer/Mail-Anonymizer.cpp
+++ Mail-Anonymizer/Mail-Anonymizer/Mail-Anonymizer.cpp
@@ -3,17 +3,63 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+
+#include <windows.h> 
 
 //using namespace std;
-
+#define BUFSIZE MAX_PATH;
 
 int hash(const char *str);
 
 int main() {
 
 
-    std::cout << hash("Blablabla");
+    //std::cout << hash("Blablabla");
+	std::string path;
+	std::cout << "Please drag & drop your exported mailbox (*.mbx) here:" << std::endl;
+	std::cin >> path;
+	
 
+	
+	try{
+		std::string line;
+		//std::ifstream myfile ("A:/DEV/swissnexCommunicationAnalysis/Mail-Anonymizer/Mail-Anonymizer/input.mbx");
+		//std::ofstream outfile ("A:/DEV/swissnexCommunicationAnalysis/Mail-Anonymizer/Mail-Anonymizer/output.mbx");
+		std::ifstream myfile (path);
+
+		std::ofstream outfile (".\\output.mbx");
+		//std::cout << outfile.good() << std::endl;
+		//std::cout << outfile.getloc() << std::endl;
+
+		if (myfile.is_open()) {
+			while (! myfile.eof() ) {
+				getline (myfile,line);
+				//std::cout << line << std::endl;
+
+				if(line.substr(0, 9).compare("Subject: ") == 0){
+					outfile << line.substr(0, 9) << hash(line.substr(9).c_str()) << std::endl;
+				}
+				else {
+					outfile << line << std::endl;
+				}
+			}
+
+		myfile.close();
+		outfile.close();
+		} 
+		else{
+			std::cout << "Bad path" << std::endl;
+		}
+	}
+	catch (std::exception& err) {
+			std::cout << err.what() << std::endl;
+	}
+
+	std::string alpha;
+	std::cout << "Type 'q' to quit." << std::endl;
+	std::cin >> alpha;
     return 0;
 }
 
